9a72ead64e36304868f39365ed93bc4cc48dcddc
fonsecadh <UO258318@uniovi.es>
2020-09-30 22:38:50 +0200

    Crawler - Working on the implementation.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,4 +1,20 @@
-import sys
+'''
+Crawler - Basic crawler for downloading HTML files from given URLs.
+Copyright (C) 2020  Hugo Fonseca Diaz
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+
+import sys, getopt
 import requests
 import time
 from bs4 import BeautifulSoup
@@ -25,11 +41,18 @@ class Crawler:
             return
         # Si podemos seguir descargando
         html = descargar(url)
+        # Comprobamos que es un HTML
+        if html == "":
+            return
         --self.max_downloads
         self.visited_urls.append(url)
-        time.sleep(seconds)
         # Llamamos a la libreria BeautifulSoup
         soup = BeautifulSoup(html.text, features="html.parser")
+        # Creamos el fichero HTML
+        self.guardar_html(soup, html.text)
+        # Dormimos el programa
+        time.sleep(seconds)
+        # Procesamos todos los links del documento HTML
         links = soup.find_all("a", href=True)
         # Procesamos los enlaces encontrados en el html
         for l in links:
@@ -51,8 +74,59 @@ class Crawler:
             return urljoin(url, link)
         return link
 
+    def guardar_html(self, soup, html):
+        # Guardamos el documento html en un fichero
+        file = open(soup.title.string.replace("/", "-") + ".html", "w+")
+        file.write(html)
+        file.close()
+
+def showHelp():
+    print("Crawler  Copyright (C) 2020  Hugo Fonseca Díaz")
+    print("This program comes with ABSOLUTELY NO WARRANTY;")
+    print("This is free software, and you are welcome to redistribute it")
+    print("under certain conditions;")
+
+    print("\nOverview:")
+    print("Normally 'Crawler' is invoked like this:")
+    print("\tcrawler.py -i URLS_FILE -m MAX_FILES -s SECONDS_TO_WAIT")
+    print("\nCommand-Line Options:")
+    print("The full format for invoking 'Crawler' is:")
+    print("\tcrawler.py OPTIONS")
+    print("\tcrawler.py -i URLS_FILE -m MAX_FILES -s SECONDS_TO_WAIT")
+    print("\n'-h'\n'--help'\n\tShow help")
+    print("\n'-i'\n'--ifile'\n\tInput filename")
+    print("\n'-m'\n'--maxfiles'\n\tMaximum number of files")
+    print("\n'-s'\n'--seconds'\n\tSeconds to wait")
 
 
+def main(argv):
+    input_file = ""
+    max_files = 0
+    seconds = 0
 
+    # We check that the input arguments are valid
+    try:
+        opts, args = getopt.getopt(argv, "hi:m:s:",["help","ifile=","maxfiles=","secs="])
+    except getopt.GetoptError:
+        showHelp()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            showHelp()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-m", "--maxfiles"):
+            max_files = int(arg)
+        elif opt in ("-s", "--secs"):
+            seconds = int(arg)
+
+    # Argument validation
+    if (inputfile == "" or max_files < 1 or seconds < 1):
+        showHelp()
+        sys.exit()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 
