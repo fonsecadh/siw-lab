e2cf1d4833987e74434225347ee1946d10b4125d
fonsecadh <UO258318@uniovi.es>
2020-10-22 18:34:33 +0200

    Index - Almost done.

--- index/index.py
+++ index/index.py
@@ -1,26 +1,70 @@
+'''
+Index - Models an inverted index to be used by a search engine.
+Copyright (C) 2020  Hugo Fonseca Diaz
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import string
 import math
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 class Index:
     """
         Modela un fichero invertido.
     """
-    def __init__(self, documents):
-        # Cargamos los documentos al diccionario
-        self.fill_index(documents)
+    def __init__(self):
         # Diccionario de terminos
         self.terms = { }
         # Numero de documentos
         self.n_documents = 0
 
-    def __fill_index__(documents):
-        pass
+    def load_document(self, id_doc, document):
+        # Carga los terminos de un documento al diccionario
+        bag = self.__string_to_bag_of_words__(document)
+        for t in bag.keys():
+            if t not in self.terms.keys():
+                # El termino no esta en el diccionario
+                post_list = { }
+                post_list[id_doc] = bag[t] # ID del documento y el TF asociado
+                self.put(IndexEntry(t, post_list)) # Insertamos la entrada
+            else:
+                # El termino ya esta en el diccionario
+                self.update_post_list(t, id_doc, bag[t])
 
     def put(self, term, entry):
         # Añadimos la entrada al diccionario
         self.terms[term] = entry
+        self.n_documents += 1
 
-    def contains_term(self, term):
+    def update_post_list(self, term, id_doc, tf):
+        # Actualiza la post_list de la entrada asociada al termino
+        self.terms[term].update_post_list(id_doc, tf)
+
+    def get_post_list(self, term):
+        # Devuelve la post-list de la entrada asociada al termino
+        self.__contains_term__(term) # Validamos el input
+        return self.terms[term].get_post_list()
+
+    def get_idf(self, term):
+        # Devuelve el idf de un termino
+        self.__contains_term__(term) # Validamos el input
+        return self.terms[term].get_idf()
+
+    def __contains_term__(self, term):
         """ 
             Comprobamos que el termino este en el diccionario. Si no esta, se
             lanza una excepcion de tipo KeyError.
@@ -30,7 +74,7 @@ class Index:
 
     def get(self, term):
         # Devuelve la entrada asociada a un termino dado
-        self.contains_term(term)
+        self.__contains_term__(term) # Validamos el input
         return self.terms[term]
 
     def __string_to_bag_of_words__(text):
@@ -60,24 +104,28 @@ class IndexEntry:
     """
         Modela una entrada del fichero invertido.
     """
-    def __init__(self, term, n_documents, post_list, idf = 0):
+    def __init__(self, term, post_list):
         """
             Constructor de la IndexEntry.
 
             Params:
                 term: termino asociado a la entrada.
-                n_documents: numero de documentos en el diccionario.
                 post_list: diccionario con identificadores de documentos donde 
                     aparece el termino y el valor TF del termino en cada uno de
                     ellos.
-                idf: valor idf del termino.
         """
         self.term = term
-        self.n_documents = n_documents
         self.post_list = post_list
-        self.idf = idf
 
-    def get_idf(self):
-        pass
+    def get_idf(self, n_documents):
+        # Evitamos una division por cero
+        n_doc_with_term = len(self.post_list) + 1
+        # Calculamos el idf
+        return math.log10(n_documents / n_doc_with_term)
 
+    def get_post_list(self):
+        return self.post_list
+
+    def update_post_list(self, id_doc, tf):
+        self.post_list[id_doc] = tf
 
