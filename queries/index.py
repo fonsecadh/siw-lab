7566c7ac7cae8002373d4ca66c669980eda32837
fonsecadh <UO258318@uniovi.es>
2020-10-27 13:58:48 +0100

    Queries - Working on the implementation

---
+++ queries/index.py
@@ -0,0 +1,130 @@
+'''
+Index - Models an inverted index to be used by a search engine.
+Copyright (C) 2020  Hugo Fonseca Diaz
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import string
+import math
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+class Index:
+    """
+        Modela un fichero invertido.
+    """
+    def __init__(self):
+        # Diccionario de terminos
+        self.terms = { }
+        # Numero de documentos
+        self.n_documents = 0
+
+    def load_document(self, id_doc, document):
+        # Carga los terminos de un documento al diccionario
+        self.n_documents += 1
+        # Creamos la bag of words
+        bag = self.__string_to_bag_of_words__(document)
+        for t in bag.keys():
+            if t not in self.terms.keys():
+                # El termino no esta en el diccionario
+                post_list = { }
+                post_list[id_doc] = bag[t] # ID del documento y el TF asociado
+                self.put(t, IndexEntry(t, post_list)) # Insertamos la entrada
+            else:
+                # El termino ya esta en el diccionario
+                self.update_post_list(t, id_doc, bag[t])
+
+    def put(self, term, entry):
+        # Añadimos la entrada al diccionario
+        self.terms[term] = entry
+
+    def update_post_list(self, term, id_doc, tf):
+        # Actualiza la post_list de la entrada asociada al termino
+        self.terms[term].update_post_list(id_doc, tf)
+
+    def get_post_list(self, term):
+        # Devuelve la post-list de la entrada asociada al termino
+        return self.get(term).get_post_list()
+
+    def get_idf(self, term):
+        # Devuelve el idf de un termino
+        return self.get(term).get_idf(self.n_documents)
+
+    def __contains_term__(self, term):
+        """ 
+            Comprobamos que el termino este en el diccionario. Si no esta, se
+            lanza una excepcion de tipo KeyError.
+        """
+        if term not in self.terms.keys():
+            raise KeyError("El termino " + term + " no tiene entradas asociadas")
+
+    def get(self, term):
+        # Devuelve la entrada asociada a un termino dado
+        self.__contains_term__(term) # Validamos el input
+        return self.terms[term]
+
+    def __string_to_bag_of_words__(self, text):
+        bag = { }
+        # Eliminamos los simbolos de puntuacion (Excepto apostrofes)
+        aux_punctuation = string.punctuation.replace("'", "")
+        translated_text = str(text).translate(str.maketrans("", "", aux_punctuation))
+        # Obtenemos los tokens
+        tokens = nltk.word_tokenize(translated_text)
+        # Procesamos los tokens
+        lemmatizer = WordNetLemmatizer()
+        for token in tokens:
+            # Lematizamos el token
+            token = lemmatizer.lemmatize(token.lower())
+            if token not in stopwords.words('english'):
+                # Si no es una stop word
+                if token not in bag:
+                    # Es la primera vez que aparece
+                    bag[token] = 1
+                else:
+                    # Ya ha aparecido antes
+                    bag[token] += 1
+        return bag
+
+
+class IndexEntry:
+    """
+        Modela una entrada del fichero invertido.
+    """
+    def __init__(self, term, post_list):
+        """
+            Constructor de la IndexEntry.
+
+            Params:
+                term: termino asociado a la entrada.
+                post_list: diccionario con identificadores de documentos donde 
+                    aparece el termino y el valor TF del termino en cada uno de
+                    ellos.
+        """
+        self.term = term
+        self.post_list = post_list
+
+    def get_idf(self, n_documents):
+        # Evitamos una division por cero
+        n_doc_with_term = len(self.post_list) + 1
+        # Calculamos el idf
+        return math.log10(n_documents / n_doc_with_term)
+
+    def get_post_list(self):
+        return self.post_list
+
+    def update_post_list(self, id_doc, tf):
+        self.post_list[id_doc] = tf
+
---
+++ queries/queries.py
@@ -0,0 +1,157 @@
+'''
+Queries - Program for retrieving information from an inverted index.
+Copyright (C) 2020  Hugo Fonseca Diaz
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import sys
+import getopt
+import re
+from index import Index
+import string
+import math
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+def get_documents(filename):
+    f = open(filename, "r")
+    docs = { }
+    for line in f.readlines():
+        aux = re.split(r"(?<=\d)\D", line, maxsplit = 1)
+        id_doc = aux[0]
+        doc_text = aux[1]
+        docs[id_doc] = doc_text
+    f.close()
+    return docs
+
+def string_to_bag_of_words(text):
+    bag = { }
+    # Eliminamos los simbolos de puntuacion (Excepto apostrofes)
+    aux_punctuation = string.punctuation.replace("'", "")
+    translated_text = str(text).translate(str.maketrans("", "", aux_punctuation))
+    # Obtenemos los tokens
+    tokens = nltk.word_tokenize(translated_text)
+    # Procesamos los tokens
+    lemmatizer = WordNetLemmatizer()
+    for token in tokens:
+        # Lematizamos el token
+        token = lemmatizer.lemmatize(token.lower())
+        if token not in stopwords.words('english'):
+            # Si no es una stop word
+            if token not in bag:
+                # Es la primera vez que aparece
+                bag[token] = 1
+            else:
+                # Ya ha aparecido antes
+                bag[token] += 1
+    return bag
+
+def get_id_docs_for_terms(terms, index):
+    # Devuelve el id de los documentos que contienen los terminos
+    id_docs = { }
+    for t in terms:
+        for id in index.get_post_list(t).keys():
+            id_docs.add(id)
+    return id_docs
+
+def get_tf(id_doc, term, index):
+    try:
+        return index.get_post_list(term)[id_doc]
+    except KeyError:
+        return 0
+
+def cosine_similarity(id_doc, bag, index):
+    dot_product = 0
+    for t in bag:
+        # Document
+        tf = get_tf(id_doc, t, index)
+        idf = index.get(t).get_idf()
+        # Bag
+        tf_bag = bag[t] / sum(bag.values())
+        dot_product += (idf * tf) * (idf * tf_bag)
+        # TODO
+
+
+def sort_by_score(id_docs, bag, index):
+    # Devuelve los id de los documentos ordenados por puntuacion
+    score = { }
+    for id_doc in id_docs:
+        score[id_doc] = cosine_similarity(id_doc, bag, index)
+    return sorted(score, key=score.get, reverse=True)
+
+
+def showHelp():
+    print("Queries  Copyright (C) 2020  Hugo Fonseca Diaz")
+    print("This program comes with ABSOLUTELY NO WARRANTY;")
+    print("This is free software, and you are welcome to redistribute it")
+    print("under certain conditions;")
+
+    print("\nOverview:")
+    print("Normally Queries is invoked like this:")
+    print("\tqueries.py -q QUERY")
+    print("\nCommand-Line Options:")
+    print("The full format for invoking Queries is:")
+    print("\tqueries.py OPTIONS")
+    print("\tqueries.py -q QUERY")
+    print("\n'-h'\n'--help'\n\tShow help")
+    print("\n'-q'\n'--query'\n\tThe query")
+
+def main(argv):
+    input_query = ""
+
+    # Comprobamos que los argumentos introducidos sean validos
+    try:
+        opts, args = getopt.getopt(argv, "hq:",["help","query="])
+    except getopt.GetoptError:
+        showHelp()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            showHelp()
+            sys.exit()
+        elif opt in ("-q", "--query"):
+            input_query = arg
+
+    # Validamos argumentos
+    if input_query == "":
+        showHelp()
+        sys.exit()
+
+    # Obtenemos la bolsa de terminos de la query
+    bag = string_to_bag_of_words(input_query) 
+
+    # Creamos el fichero invertido
+    index = Index()
+
+    # Extraemos los documentos
+    documents = get_documents("cran-1400.txt")
+
+    # Rellenamos el indice
+    for id_doc in documents.keys():
+        index.load_document(id_doc, documents[id_doc])
+
+    # Obtenemos el id de los documentos que contienen los terminos
+    id_docs = get_id_docs_for_terms(bag.keys(), index)
+
+    # Obtenemos el id de los documentos ordenados por puntuacion
+    id_docs_sorted = sort_by_score(id_docs, bag, index) 
+    
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
