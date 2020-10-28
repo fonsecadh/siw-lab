65e1fb2a400ff11c3eeb9d8186362ac017e7f7ad
fonsecadh <UO258318@uniovi.es>
2020-10-28 22:40:18 +0100

    Queries - Working on the web service implementation.

--- queries/queries.py
+++ queries/queries.py
@@ -19,6 +19,8 @@ import re
 from index import Index
 import string
 import math
+import web
+from web import form
 import nltk
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -26,6 +28,44 @@ nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+# Definimos el buscador
+searcher = None # Se inicializa en el main
+
+# Ruta de las plantillas
+render = web.template.render('templates/')
+
+# Estructura de URLS
+urls = (
+  '/', 'index',
+  '/results', 'results'
+)
+
+# Formulario para las consultas
+query_form = form.Form(
+    form.Textbox("query", description="Query"),
+    validators = [
+        form.Validator("A query must be specified", lambda i: i.query == "")
+    ]
+)
+
+# Clases para el servicio web
+class index:
+    def GET(self):
+        return render.index(query_form())
+    
+    def POST(self):
+        f = query_form()
+        if not f.validates():
+            return render.index(query_form())
+        else:
+            input_query = f["query"].value
+            return render.results(searcher.process_query(input_query))
+
+class results:
+    def GET(self):
+        return render.results()
+
+# Funciones y clases para resolver el problema
 def get_documents(filename):
     f = open(filename, "r")
     docs = { }
@@ -36,72 +76,6 @@ def get_documents(filename):
         docs[id_doc] = doc_text
     f.close()
     return docs
-
-def string_to_bag_of_words(text):
-    bag = { }
-    # Eliminamos los simbolos de puntuacion (Excepto apostrofes)
-    aux_punctuation = string.punctuation.replace("'", "")
-    translated_text = str(text).translate(str.maketrans("", "", aux_punctuation))
-    # Obtenemos los tokens
-    tokens = nltk.word_tokenize(translated_text)
-    # Procesamos los tokens
-    lemmatizer = WordNetLemmatizer()
-    for token in tokens:
-        # Lematizamos el token
-        token = lemmatizer.lemmatize(token.lower())
-        if token not in stopwords.words('english'):
-            # Si no es una stop word
-            if token not in bag:
-                # Es la primera vez que aparece
-                bag[token] = 1
-            else:
-                # Ya ha aparecido antes
-                bag[token] += 1
-    return bag
-
-def get_id_docs_for_terms(terms, index):
-    # Devuelve el id de los documentos que contienen los terminos
-    id_docs = set()
-    for t in terms:
-        for id in index.get_post_list(t).keys():
-            id_docs.add(id)
-    return id_docs
-
-def get_tf(id_doc, term, index):
-    try:
-        return index.get_post_list(term)[id_doc]
-    except KeyError:
-        return 0
-
-def cosine_similarity(id_doc, bag, index):
-    dot_product = 0
-    aux_doc = 0
-    aux_bag = 0
-    for t in bag:
-        # Document
-        tf = get_tf(id_doc, t, index)
-        idf = index.get_idf(t)
-        # Bag
-        tf_bag = bag[t] / sum(bag.values())
-        # tf * idf
-        tfidf_doc = idf * tf
-        tfidf_bag = idf * tf_bag
-        # Dot product
-        dot_product += (tfidf_doc) * (tfidf_bag)
-        # Para luego calcular la magnitud
-        aux_doc += math.pow(tfidf_doc, 2)
-        aux_bag += math.pow(tfidf_bag, 2)
-    # Magnitudes
-    magnitude_doc = math.sqrt(aux_doc)
-    magnitude_bag = math.sqrt(aux_bag)
-    return dot_product / (magnitude_doc * magnitude_bag)
-
-def sort_by_score(id_docs, bag, index):
-    # Devuelve los id de los documentos ordenados por puntuacion
-    score = { }
-    for id_doc in id_docs:
-        score[id_doc] = cosine_similarity(id_doc, bag, index)
-    return sorted(score, key=score.get, reverse=True)
 
 def show_results(results, results_to_show = None):
     # Validaciones
@@ -124,6 +98,90 @@ def show_results(results, results_to_show = None):
         else:
             return
 
+class Searcher:
+    def __init__(self, index):
+        self.index = index
+
+    def __string_to_bag_of_words__(text):
+        bag = { }
+        # Eliminamos los simbolos de puntuacion (Excepto apostrofes)
+        aux_punctuation = string.punctuation.replace("'", "")
+        translated_text = str(text).translate(str.maketrans("", "", aux_punctuation))
+        # Obtenemos los tokens
+        tokens = nltk.word_tokenize(translated_text)
+        # Procesamos los tokens
+        lemmatizer = WordNetLemmatizer()
+        for token in tokens:
+            # Lematizamos el token
+            token = lemmatizer.lemmatize(token.lower())
+            if token not in stopwords.words('english'):
+                # Si no es una stop word
+                if token not in bag:
+                    # Es la primera vez que aparece
+                    bag[token] = 1
+                else:
+                    # Ya ha aparecido antes
+                    bag[token] += 1
+        return bag
+
+    def __get_id_docs_for_terms__(terms, index):
+        # Devuelve el id de los documentos que contienen los terminos
+        id_docs = set()
+        for t in terms:
+            for id in index.get_post_list(t).keys():
+                id_docs.add(id)
+        return id_docs
+
+    def __get_tf__(id_doc, term, index):
+        try:
+            return index.get_post_list(term)[id_doc]
+        except KeyError:
+            return 0
+
+    def __cosine_similarity__(id_doc, bag, index):
+        dot_product = 0
+        aux_doc = 0
+        aux_bag = 0
+        for t in bag:
+            # Document
+            tf = __get_tf__(id_doc, t, index)
+            idf = index.get_idf(t)
+            # Bag
+            tf_bag = bag[t] / sum(bag.values())
+            # tf * idf
+            tfidf_doc = idf * tf
+            tfidf_bag = idf * tf_bag
+            # Dot product
+            dot_product += (tfidf_doc) * (tfidf_bag)
+            # Para luego calcular la magnitud
+            aux_doc += math.pow(tfidf_doc, 2)
+            aux_bag += math.pow(tfidf_bag, 2)
+        # Magnitudes
+        magnitude_doc = math.sqrt(aux_doc)
+        magnitude_bag = math.sqrt(aux_bag)
+        return dot_product / (magnitude_doc * magnitude_bag)
+
+    def __sort_by_score__(id_docs, bag, index):
+        # Devuelve los id de los documentos ordenados por puntuacion
+        score = { }
+        for id_doc in id_docs:
+            score[id_doc] = cosine_similarity(id_doc, bag, index)
+        return sorted(score, key=score.get, reverse=True)
+
+    def process_query(self, query):
+        # Obtenemos la bolsa de terminos de la query
+        bag = __string_to_bag_of_words__(query) 
+
+        # Obtenemos el id de los documentos que contienen los terminos
+        id_docs = __get_id_docs_for_terms__(bag.keys(), self.index)
+
+        # Obtenemos el id de los documentos ordenados por puntuacion
+        id_docs_sorted = __sort_by_score__(id_docs, bag, self.index)
+
+        # Devolvemos los resultados
+        return id_docs_sorted
+
+
 def showHelp():
     print("Queries  Copyright (C) 2020  Hugo Fonseca Diaz")
     print("This program comes with ABSOLUTELY NO WARRANTY;")
@@ -141,29 +199,6 @@ def showHelp():
     print("\n'-q'\n'--query'\n\tThe query")
 
 def main(argv):
-    input_query = ""
-
-    # Comprobamos que los argumentos introducidos sean validos
-    try:
-        opts, args = getopt.getopt(argv, "hq:",["help","query="])
-    except getopt.GetoptError:
-        showHelp()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            showHelp()
-            sys.exit()
-        elif opt in ("-q", "--query"):
-            input_query = arg
-
-    # Validamos argumentos
-    if input_query == "":
-        showHelp()
-        sys.exit()
-
-    # Obtenemos la bolsa de terminos de la query
-    bag = string_to_bag_of_words(input_query) 
-
     # Creamos el fichero invertido
     index = Index()
 
@@ -174,14 +209,9 @@ def main(argv):
     for id_doc in documents.keys():
         index.load_document(id_doc, documents[id_doc])
 
-    # Obtenemos el id de los documentos que contienen los terminos
-    id_docs = get_id_docs_for_terms(bag.keys(), index)
+    # Creamos el buscador
+    searcher = Searcher(index)
 
-    # Obtenemos el id de los documentos ordenados por puntuacion
-    id_docs_sorted = sort_by_score(id_docs, bag, index) 
-
-    # Mostramos los resultados
-    show_results(id_docs_sorted)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
